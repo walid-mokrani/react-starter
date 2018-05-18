f0e020ce31a2890ce2333153a440043dcd8cf97b
walid mokrani <wal.mokrani@gmail.com>
2018-05-18 01:04:35 +0100

    chore: add js linter and prettier

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,4 @@
+// https://github.com/marionebl/commitlint
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 }
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+// Prettier configuration
+// https://prettier.io/docs/en/configuration.html
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+}
