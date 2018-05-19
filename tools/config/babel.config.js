0c724a531ee8746f66a22f930365887a406a10b6
walid mokrani <wal.mokrani@gmail.com>
2018-05-19 13:49:03 +0100

    chore: optimize react build

--- tools/config/babel.config.js
+++ tools/config/babel.config.js
@@ -34,6 +34,17 @@ const config = {
     // https://github.com/babel/babel/tree/master/packages/babel-preset-react
     ['@babel/preset-react', { development: isDebug }],
   ],
+  plugins: [
+    // Treat React JSX elements as value types and hoist them to the highest scope
+    // https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-react-constant-elements
+    ...(isDebug ? [] : ['@babel/transform-react-constant-elements']),
+    // Replaces the React.createElement function with one that is more optimized for production
+    // https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-react-inline-elements
+    ...(isDebug ? [] : ['@babel/transform-react-inline-elements']),
+    // Remove unnecessary React propTypes from the production build
+    // https://github.com/oliviertassinari/babel-plugin-transform-react-remove-prop-types
+    ...(isDebug ? [] : ['transform-react-remove-prop-types']),
+  ],
 }
 
 export default config
