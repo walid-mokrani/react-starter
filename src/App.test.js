48c8926e4bac062a897321e76654394eb91f71f5
walid mokrani <wal.mokrani@gmail.com>
2018-05-19 12:44:02 +0100

    chore: add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
 /* eslint-env jest */
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import App from './App'
+
+describe('App', () => {
+  test('renders children correctly', () => {
+    const wrapper = TestRenderer.create(<App />).toJSON()
+
+    expect(wrapper).toMatchSnapshot()
+  })
 })
